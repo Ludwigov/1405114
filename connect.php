a6bde676c5394b592e5abf20074d751fb283318e
1405114 <a@a.com>
2016-08-18 12:09:06 +0100

    x

---
+++ connect.php
@@ -0,0 +1,13 @@
+<?php
+
+define ('DB_SERVER', "ap-cdbr-azure-east-c.cloudapp.net");
+define ('DB_USERNAME', "b419ceeda1d055");
+define ('DB_PASSWORD', "5b8fbe3f");
+define ('DB_DATABASE', "CMM007ALDB-1405114");
+
+$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+
+if ($db->connect_error) {
+    die("Connection failed: " . $db->connect_error);
+}
+?>
